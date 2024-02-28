cc8584cac1a9cc2eda17e8cd33aebd84ae4c9eea
Sibren Reekers <sibrenreekers@gmail.com>
2024-02-28 10:30:06 +0100

    Added some comment for github actions testing

--- src/main/java/com/shbreekers/springtest/nextdb/DatabaseLoader.java
+++ src/main/java/com/shbreekers/springtest/nextdb/DatabaseLoader.java
@@ -20,9 +20,5 @@ public class DatabaseLoader implements CommandLineRunner {
     public void run(String... strings) throws Exception {
 //        // Voorbeeld van het opslaan van een Athlete
 //        athleteRepository.save(new Athlete("Sibren", "Reekers", "sibrenreekers@gmail.com"));
-//
-//        // Voorbeeld van het opslaan van een Note
-//        // Zorg ervoor dat je de Note klasse en NoteRepository al hebt gedefinieerd
-//        noteRepository.save(new Note("Mijn eerste notitie", "Dit is de inhoud van mijn eerste notitie."));
     }
 }
